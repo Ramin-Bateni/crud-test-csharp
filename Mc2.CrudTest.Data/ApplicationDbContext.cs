f4ac4139846e322aa67f8a1fd7cb37313d5cdc80
Ramin-Bateni <ram_20designer@yahoo.com>
2022-12-28 19:08:51 +0330

    New projects added and solution made more ready for coding

---
+++ Mc2.CrudTest.Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Mc2.CrudTest.ApplicationServices.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mc2.CrudTest.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Customer> Customer { get; set; }
+    }
+}
---
+++ Mc2.CrudTest.Data/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mc2.CrudTest.ApplicationServices;
+
+namespace Mc2.CrudTest.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<int> SaveAsync()
+        {
+            return _context.SaveChangesAsync();
+        }
+    }
+}
